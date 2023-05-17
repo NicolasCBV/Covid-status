d0b5e897c01d98771eb19bef1b6e7736da2a2f81
Nícolas <92616145+NicolasCBV@users.noreply.github.com>
2023-05-17 03:14:55 -0300

    Update Information.tsx

--- src/components/Information.tsx
+++ src/components/Information.tsx
@@ -5,7 +5,7 @@ export function Information() {
         Este site foi construido com a função de monitorar as estatísticas da covid-19, contando as mortes, e os números de casos catalogados nos últimos 30 dias por meio da API do site <a className="text-seaGreenCrayola-250 duration-300 hover:text-seaGreenCrayola-550" href="https://disease.sh/"  target="_blank">https://disease.sh/</a>, Caso você encontre alguma informação incongruente nos gráficos, favor contatar o email <a className="text-seaGreenCrayola-250 duration-300 hover:text-seaGreenCrayola-550" href="mailto:nicolascbv700@gmail.com"  target="_blank">nicolascbv700@gmail.com</a>, desde já, agradeço pela colaboração.
       </p>
       <p className="mb-6">
-        Ademais, caso você se sinta curioso, sinta-se a vontade de checar mais projetos feitos por mim no github - link no menu acima - ou checar as minhas outras redes sociais:
+        Ademais, caso você se sinta curioso, sinta-se a vontade de checar mais projetos feitos por mim no github, link no menu acima.
       </p>
     </div>
   )
